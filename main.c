c48219da8ba0093b9f8be8b7f847e09d5ffec8f1
git-sandip <sandipsapkota9844@gmail.com>
2023-07-13 21:49:02 +0545

    added file handeling system

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include<string.h>
 void Introduction(){
     printf("\t\t---Welcome To Console Based Movie Tickteing System!-----\n");
-    printf("\t\t----Developed by Sandip,Ragav,Subakhar & Sulav----\n");
+    printf("\t\t----Developed by Sandip,Raghav,Subakhar & Sulav----\n");
 }
 void UserInformationInput(){
     char name[100];
@@ -17,7 +17,18 @@ void UserInformationInput(){
     printf("Your name is %s\n",name); 
     printf("Your email is %s\n",email);
     printf("Your number is %d\n",number);
-}
+FILE *fptr;
+   fptr = fopen("data/users.txt", "r+"); 
+   if(fptr == NULL){
+  printf("Error! Could not open file\n");
+                exit(-1);
+   }else{
+fprintf(fptr,"%s\n",name);
+fprintf(fptr,"%s\n",email);
+fprintf(fptr,"%d\n",number);
+   }
+fclose(fptr);}
+
 int main()
 {
 Introduction();
