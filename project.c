3bbcd0feade789dad5d18dd2b4b18bb7cb49dc04
git-sandip <sandipsapkota9844@gmail.com>
2023-07-25 00:18:46 +0545

    implemented wiriting candidates names and dispalying it using file handeling

--- project.c
+++ project.c
@@ -9,7 +9,6 @@ typedef struct Candidates {
 
 void CandidateDetailsInput(can can[], int n) {
     int i;
-
     FILE* fptr;
     fptr = fopen("data/candidates.txt", "a");
     if (fptr == NULL) {
@@ -17,29 +16,41 @@ void CandidateDetailsInput(can can[], int n) {
         exit(1);
     } else {
         for (i = 0; i < n; i++) {
-            printf("Enter the candidate id (between 1 and 100):\n");
-            scanf("%d", &can[i].id);
-
-            // Input validation for candidate ID
-            while (can[i].id < 1 || can[i].id > 100) {
-                printf("Invalid candidate ID. Please enter a valid ID (between 1 and 100):\n");
+            do {
+                printf("Enter the candidate id (between 1 and 100):\n");
                 scanf("%d", &can[i].id);
-            }
+
+                if (can[i].id < 1 || can[i].id > 100) {
+                    printf("Invalid candidate ID. Please enter a valid ID (between 1 and 100):\n");
+                }
+            } while (can[i].id < 1 || can[i].id > 100);
 
             printf("Enter the candidate name (up to 49 characters):\n");
-            // Using fgets to prevent buffer overflow
-            fflush(stdin);
-            fgets(can[i].name, sizeof(can[i].name), stdin);
-            can[i].name[strcspn(can[i].name, "\n")] = '\0'; // Removing the newline character
-
+            scanf(" %[^\n]", can[i].name);
+            
             // Write candidate details to the file
             fprintf(fptr, "Id: %d\tName: %s\n", can[i].id, can[i].name);
         }
         fclose(fptr);
     }
 }
+void displayCandidates() {
+    printf("ID\tCandidate Name\n");
 
+    FILE* fptr;
+    fptr = fopen("data/candidates.txt", "r");
+    if (fptr == NULL) {
+        printf("Error! Could not open file\n");
+        exit(1);
+    }
 
+    char line[100];
+    while (fgets(line, sizeof(line), fptr) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fptr);
+}
 
 
 
@@ -48,17 +59,13 @@ void Introduction(){
     printf("\t\t----Developed by Sandip,Raghav,Subakhar & Sulav----\n");
 }
 
-
-
-
-
 int main()
 {
     int n;
     printf("How many candidates do you want to enter?\n");
     scanf("%d", &n);
-
     can candidates[n];
     CandidateDetailsInput(candidates, n);
+    displayCandidates();
     return 0;
 }
